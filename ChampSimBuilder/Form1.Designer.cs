139b9d4bfefe0e583bbe0ea86da73836a7b5d942
tsakas96 <aristos_fox@hotmail.com>
2018-10-28 21:42:51 +0200

    Day One

--- ChampSimBuilder/Form1.Designer.cs
+++ ChampSimBuilder/Form1.Designer.cs
@@ -28,12 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CPU:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1",
+            "4",
+            "8"});
+            this.comboBox1.Location = new System.Drawing.Point(50, 6);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(51, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(495, 261);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "ChampSimBuilder";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
     }
 }
 
--- ChampSimBuilder/Form1.cs
+++ ChampSimBuilder/Form1.cs
@@ -12,9 +12,72 @@ namespace ChampSimBuilder
 {
     public partial class Form1 : Form
     {
+        public Button[] buttonArray = new Button[8];
+        public Label[] labelArray = new Label[8];
+        public TextBox[] textBoxArray = new TextBox[8];
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void btn_cpu_Click(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+            int index = int.Parse(button.Name[button.Name.Length - 1].ToString());
+            DialogResult result = openFileDialog1.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                textBoxArray[index].Text = openFileDialog1.FileName;
+                //fileName = fileName.Substring(openFileDialog1.FileName.LastIndexOf("\\") + 1);
+                //text = File.ReadAllText(openFileDialog1.FileName);
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                Controls.Remove(buttonArray[i]);
+                Controls.Remove(labelArray[i]);
+                Controls.Remove(textBoxArray[i]);
+            }
+
+            for (int i = 0; i < int.Parse(comboBox1.SelectedItem.ToString()); i++)
+            {
+
+                labelArray[i] = new Label();
+                buttonArray[i] = new Button();
+                textBoxArray[i] = new TextBox();
+
+
+                labelArray[i].Size = new Size(40, 15);
+                buttonArray[i].Size = new Size(70, 20);
+                textBoxArray[i].Size = new Size(170, 10);
+                
+                labelArray[i].Name = "lbl_cpu_" + i;
+                buttonArray[i].Name = "btn_cpu_" + i;
+                textBoxArray[i].Name = "txt_cpu_" + i;
+
+                labelArray[i].Text = "CPU" + i + ":";
+                buttonArray[i].Text = "Browse..";
+                textBoxArray[i].Text = "File location for trace file of CPU" + i;
+
+                labelArray[i].Location = new Point(125, 10 + (i * 25));
+                textBoxArray[i].Location = new Point(175, 5 + (i * 25));
+                buttonArray[i].Location = new Point(350, 5 + (i * 25));
+
+                buttonArray[i].Click += btn_cpu_Click;
+
+                Controls.Add(labelArray[i]);
+                Controls.Add(buttonArray[i]);
+                Controls.Add(textBoxArray[i]);
+            }
         }
     }
 }
